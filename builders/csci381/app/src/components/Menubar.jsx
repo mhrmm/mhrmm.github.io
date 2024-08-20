7135f012c79374462f78b2e207f2cb74ff63f1a2
Mark Hopkins <mark.andrew.hopkins@gmail.com>
2024-08-20 15:34:25 -0400

    Made it easier to click on menu links.

--- builders/csci381/app/src/components/Menubar.jsx
+++ builders/csci381/app/src/components/Menubar.jsx
@@ -11,11 +11,11 @@ const Menubar = () => (
     width: "100%",
     textAlign: "center"
   }}>
-    <Link to={{ pathname: '/dlamp1' }}><div className="menubar-link" style={{flexGrow: 1, flexShrink: 1}}>1</div></Link> 
+    <Link style={{flexGrow: 1, flexShrink: 1}} to={{ pathname: '/dlamp1' }}><div className="menubar-link" >1</div></Link> 
     <div className="textcolor">|</div>
-    <Link to={{ pathname: '/dlamp2' }}><div className="menubar-link" style={{flexGrow: 1, flexShrink: 1}}>2</div></Link>
+    <Link style={{flexGrow: 1, flexShrink: 1}} to={{ pathname: '/dlamp2' }}><div className="menubar-link">2</div></Link>
     <div className="textcolor">|</div>
-    <Link to={{ pathname: '/dlamp3' }}><div className="menubar-link" style={{flexGrow: 1, flexShrink: 1}}>3</div></Link>
+    <Link style={{flexGrow: 1, flexShrink: 1}} to={{ pathname: '/dlamp3' }}><div className="menubar-link">3</div></Link>
   </div>
 )
 
